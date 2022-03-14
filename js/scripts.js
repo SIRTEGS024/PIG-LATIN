4a34bdec1c41c396278fdea86a7c5cb260278000
tega <kesienametitiri024@gmail.com>
2022-03-14 15:54:46 +0100

    add user interface logic on html and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 jQuery(document).ready(function () {
   // Business Logic
-  let sentence = prompt("Enter a sentence:");
+  $("form#Translator").submit(function(event) {
+    event.preventDefault();
+  let sentence = $("input#input1").val()
   let vowelSentence = sentence.charAt(0);
   let quSentence = sentence.slice(0,2);
   let remSentence = sentence.slice(2)
   let remSentence2 = sentence.slice(1)
+  
+// User Interface Logic 
+  let result;
   if (vowelSentence === 'a' || vowelSentence === 'e' || vowelSentence === 'i' || vowelSentence === 'o' || vowelSentence === 'u') {
-    alert(sentence.concat("acedu"))
+   result = sentence.concat("acedu")
   }
   else if (quSentence === 'qu' ) {
-    alert(remSentence.concat(quSentence, "ayac"))
+   result = remSentence.concat(quSentence, "ayac")
   }
   else {
-    alert(remSentence2.concat(vowelSentence,"ayac"))
+   result = remSentence2.concat(vowelSentence,"ayac")
   }
+$("#output").text(result);
 });
-// User Interface Logic
+});
+
 
